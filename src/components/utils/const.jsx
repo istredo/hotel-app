3954e77c640dc53e6a3d88bc23d65b48f33c7918
istredo <uvarov6799@gmail.com>
2023-12-26 17:35:09 +0300

    fix: add test key

--- src/components/utils/const.jsx
+++ src/components/utils/const.jsx
@@ -2,6 +2,6 @@ export const CHAT_ID = '-4022855160';
 
 export const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-export const API_URL = `https://api.telegram.org/bot${API_KEY}/sendMessage`;
+export const API_URL = `https://api.telegram.org/bot6917558281:AAHTqS7g68nKDmE4OTpPssaomdYdJYb_WVk/sendMessage`;
 
 export const CAROUSEL_WIDTH = 30; // ширина блока карусели
